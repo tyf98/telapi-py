f00cae6cb30d2f3d7d870be9326f73eb881cbcf2
tyf98 <49827907+tyf98@users.noreply.github.com>
2024-02-09 12:36:46 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -11,23 +11,23 @@ from staticmap import StaticMap, CircleMarker
 app = FastAPI()
 
 @app.get("/", response_class=Response)
-def qrdemo(color: str = '#7A663C', logolocation: str = 'online', logourl: str = "https://i0.wp.com/godofwealth.co/wp-content/uploads/2023/10/happy-chinese-new-year-2024-thumb.jpg"):
+def qrdemo(color: str = '#7A663C', logourl: str = "https://i0.wp.com/godofwealth.co/wp-content/uploads/2023/10/happy-chinese-new-year-2024-thumb.jpg"):
     # Open QR code image
     qr_image = Image.open(generate_qr('Hello World!', color)).convert('RGBA')  # Convert QR code to RGBA mode
 
     # Call the overlay_qr_code function with the desired percentage
-    qr_image = overlay_qr_code(qr_image, fetch_logo(logolocation, logourl), percentageOfQrCode)
+    qr_image = overlay_qr_code(qr_image, fetch_logo(logourl), percentageOfQrCode)
 
     # Save result
     return save_result(qr_image)
 
 @app.get("/qrcode", response_class=Response)
-def qrdemo(data:str, color: str = '#7A663C', logolocation: str = 'online', logourl: str ='https://www.sgcarmart.com/_next/image?url=https%3A%2F%2Fi.i-sgcm.com%2Fnews%2Farticle_news%2F2020%2F22464_3_l.jpg&w=1920&q=75'):
+def qrdemo(data:str, color: str = '#7A663C', logourl: str ='https://www.sgcarmart.com/_next/image?url=https%3A%2F%2Fi.i-sgcm.com%2Fnews%2Farticle_news%2F2020%2F22464_3_l.jpg&w=1920&q=75'):
     # Open QR code image
     qr_image = Image.open(generate_qr(data, color)).convert('RGBA')  # Convert QR code to RGBA mode
 
     # Call the overlay_qr_code function with the desired percentage
-    qr_image = overlay_qr_code(qr_image, fetch_logo(logolocation, logourl), percentageOfQrCode)
+    qr_image = overlay_qr_code(qr_image, fetch_logo(logourl), percentageOfQrCode)
 
     # Save result
     return save_result(qr_image)
@@ -50,7 +50,7 @@ def generate_map(deviceLat: float, deviceLon: float):
     return save_result(image)
 
 @app.get("/vcard", response_class=Response)
-def vcard_qr(first_name: str, last_name: str, organisation: str, title: str, email: str, phone: str, street: str, city: str, state: str, country: str, postal: str, website: str, color: str = '#7A663C', logolocation: str = 'online', logourl: str = 'https://www.sgcarmart.com/_next/image?url=https%3A%2F%2Fi.i-sgcm.com%2Fnews%2Farticle_news%2F2020%2F22464_3_l.jpg&w=1920&q=75'):
+def vcard_qr(first_name: str, last_name: str, organisation: str, title: str, email: str, phone: str, street: str, city: str, state: str, country: str, postal: str, website: str, color: str = '#7A663C', logourl: str = 'https://www.sgcarmart.com/_next/image?url=https%3A%2F%2Fi.i-sgcm.com%2Fnews%2Farticle_news%2F2020%2F22464_3_l.jpg&w=1920&q=75'):
     # Generate vCard data manually
     vcard_data = f'BEGIN:VCARD\nVERSION:3.0\nN:{last_name};{first_name}\nFN:{first_name} {last_name}\nORG:{organisation}\nTITLE:{title}\nEMAIL:{email}\nTEL;TYPE=cell:{phone}\nADR;TYPE=Work:;;{street};{city};{state};{postal};{country}\nURL:{website}\nEND:VCARD'
 
@@ -58,7 +58,7 @@ def vcard_qr(first_name: str, last_name: str, organisation: str, title: str, ema
     qr_image = Image.open(generate_qr(vcard_data, color)).convert('RGBA')  # Convert QR code to RGBA mode
 
     # Call the overlay_qr_code function with the desired percentage
-    qr_image = overlay_qr_code(qr_image, fetch_logo(logolocation, logourl), percentageOfQrCode)
+    qr_image = overlay_qr_code(qr_image, fetch_logo(logourl), percentageOfQrCode)
     
     # Save result
     return save_result(qr_image)
@@ -90,23 +90,20 @@ def overlay_qr_code(qr_image, overlay_image, percentageOfQrCode):
     # Return the modified qr_image
     return qr_image
 
-def fetch_logo(logolocation: str, logourl: str):
-    if logolocation == 'online':
-        # Download overlay image from URL
-        try:
-            response = requests.get(logourl)
-        except requests.exceptions.RequestException as e:
-            logging.error(f" Invalid URL or base64 image: {e}")
-            return JSONResponse(content={"error": "Invalid URL or base64 image"})
+def fetch_logo(logourl: str):
+    # Download overlay image from URL
+    try:
+        response = requests.get(logourl)
+    except requests.exceptions.RequestException as e:
+        logging.error(f" Invalid URL or base64 image: {e}")
+        return JSONResponse(content={"error": "Invalid URL or base64 image"})
 
-        # Check if the URL points to an image
-        if 'content-type' not in response.headers or 'image' not in response.headers['content-type']:
-            logging.error("  URL does not point to an image")
-            return JSONResponse(content={"error": "URL does not point to an image"})
-        
-        return Image.open(BytesIO(response.content)).convert('RGBA')  # Convert overlay image to RGBA mode
-    else:
-        return Image.open(logourl).convert('RGBA')  # Convert overlay image to RGBA mode
+    # Check if the URL points to an image
+    if 'content-type' not in response.headers or 'image' not in response.headers['content-type']:
+        logging.error("  URL does not point to an image")
+        return JSONResponse(content={"error": "URL does not point to an image"})
+    
+    return Image.open(BytesIO(response.content)).convert('RGBA')  # Convert overlay image to RGBA mode
     
 def save_result(qr_image: Image):
     # Save result
