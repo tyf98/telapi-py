9c8085a0567b9820080d98c35a5ab4e5aecbcd9b
tyf98 <49827907+tyf98@users.noreply.github.com>
2024-05-06 11:06:11 +0800

    Text Overlay on logo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Response
 from starlette.responses import StreamingResponse
 from io import BytesIO
 import requests
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import segno
 import logging
 from staticmap import StaticMap, CircleMarker
@@ -10,7 +10,7 @@ from geopy.geocoders import Nominatim
 
 app = FastAPI()
 
-defaultLogo = "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpiha9Xx-7QMwN6lO2vUbnR7m4PZLu9gzvZ_wQBHZ6txL3ED2E0vY6MFvZKTlYH3JpqGriMsvZOcZl7-AwJxIsl7cP-jzUkg=w2864-h1430-v0"
+defaultLogo = "https://i.ibb.co/WPQSS4d/Corp-Rectangular.png"
 
 @app.get("/", response_class=Response)
 def qrdemo(color: str = '#7A663C', logourl: str = defaultLogo, percentageOfQrCode: float=0.7):
@@ -24,7 +24,7 @@ def qrdemo(color: str = '#7A663C', logourl: str = defaultLogo, percentageOfQrCod
     return save_result(qr_image)
 
 @app.get("/qrcode", response_class=Response)
-def qrcode(data:str, color: str = '#7A663C', logourl: str = defaultLogo, percentageOfQrCode: float=0.3):
+def qrcode(data:str, color: str = '#7A663C', logourl: str = defaultLogo, percentageOfQrCode: float=0.3, scdLabel: str | None = None):
 
     # Check if logourl is default
     if logourl == defaultLogo:
@@ -34,7 +34,7 @@ def qrcode(data:str, color: str = '#7A663C', logourl: str = defaultLogo, percent
     qr_image = Image.open(generate_qr(data, color)).convert('RGBA')  # Convert QR code to RGBA mode
 
     # Call the overlay_qr_code function with the desired percentage
-    qr_image = overlay_qr_code(qr_image, fetch_logo(logourl), percentageOfQrCode)
+    qr_image = overlay_qr_code(qr_image, fetch_logo(logourl), percentageOfQrCode, scdLabel)
 
     # Save result
     return save_result(qr_image)
@@ -75,7 +75,7 @@ def generate_qr(data: str, color: str = '#000000'):
     qr_buffer.seek(0)
     return qr_buffer
 
-def overlay_qr_code(qr_image, overlay_image, percentageOfQrCode):
+def overlay_qr_code(qr_image, overlay_image, percentageOfQrCode, scdLabel=None):
     # Check if overlay_image is not None
     if overlay_image is None:
         logging.error("Overlay image is not available")
@@ -93,6 +93,33 @@ def overlay_qr_code(qr_image, overlay_image, percentageOfQrCode):
 
     # Overlay image on QR code
     qr_image.paste(overlay_image, position, overlay_image)  # Use overlay_image as mask for transparency
+
+    if scdLabel is not None:
+        logging.info("SCD Label is not none.")
+        
+        # Create ImageDraw object
+        draw = ImageDraw.Draw(qr_image)
+
+        # Specify font-size and type
+        font_size = 30
+        font = ImageFont.truetype("./arialbd.ttf", font_size)
+
+        # Calculate width and height of the text to center it
+        text_length = draw.textlength(scdLabel, font)
+
+        # If the text is too wide for the overlay image, decrease the font size until it fits
+        while text_length > overlay_width/2:
+            font_size -= 1
+            font = ImageFont.truetype("./arialbd.ttf", font_size)
+            text_length = draw.textlength(scdLabel, font)
+
+        # Calculate position for the text (centered horizontally, 10% from the bottom vertically)
+        text_x = (qr_image.width - text_length) // 2
+        text_y = position[1] + overlay_height - int(0.27 * overlay_height)
+
+        # Apply text overlay
+        draw.text((text_x, text_y), scdLabel, fill="#7A663C", font=font)
+        
 
     # Return the modified qr_image
     return qr_image
